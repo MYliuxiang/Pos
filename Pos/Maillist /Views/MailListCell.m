638930c6dfd5f650c108e2c35d1da4f2a3d79d9a
刘翔 <liuxiang@liuxiangdeMacBook-Pro.local>
2021-06-27 01:02:51 +0800

    2

---
+++ Pos/Maillist /Views/MailListCell.m
@@ -0,0 +1,23 @@
+//
+//  MailListCell.m
+//  Pos
+//
+//  Created by 刘翔 on 2021/6/27.
+//
+
+#import "MailListCell.h"
+
+@implementation MailListCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
